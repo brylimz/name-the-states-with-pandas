03ee92d17047f6119829f465a61b430fa134c43f
brylimz <superuser@brylimz.com>
2021-12-14 14:28:06 +0700

    huhu

---
+++ main.py
@@ -0,0 +1,30 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+    print(answer_state)
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+# if answer_state is on the states in all the states of the 50_states csv
+    # if they got it right:
+    # create a turtle to write the name of the state at the state's x y coordinate
+
+screen.exitonclick()
