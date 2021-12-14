92424c5a27e2786923beccea8f95aaddc914d4f1
brylimz <superuser@brylimz.com>
2021-12-14 15:05:03 +0700

    completed

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+
 guessed_states = []
 
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
-    print(answer_state)
-
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -23,8 +30,8 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
-# if answer_state is on the states in all the states of the 50_states csv
+
+    # if answer_state is on the states in all the states of the 50_states csv
     # if they got it right:
     # create a turtle to write the name of the state at the state's x y coordinate
-
-screen.exitonclick()
+# states_to_learn_csv
